ee8632ba63fc2b4867341088bb2383e0786f52b3
LiliValdo <142357070+A01712128@users.noreply.github.com>
2024-06-10 10:28:08 -0600

    Update Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -1,4 +1,18 @@
-#ifndef USUARIO_H
+/*
+ *
+ * Proyecto Gestor de tareas clase Usuario
+ * Liliana Valdovinos Catarino
+ * A01712128
+ * 10/06/2024
+ *
+ * Esta clase define un objeto de tipo Usuario que contiene el nombre, correo,
+ * y lista de tareas de un usuario. Permite establecer y obtener los datos del 
+ * usuario, así como acceder a la lista de tareas del usuario. Esta clase es 
+ * utilizada por la función principal del programa para gestionar los datos 
+ * del usuario y sus tareas.
+ */
+
+#ifndef USUARIO_H // Si USUARIO_H no ha sido definido,
 #define USUARIO_H
 
 #include "ListaTareas.h"
@@ -7,16 +21,16 @@ using namespace std;
 
 class Usuario {
 private:
-    string nombre;
-    string correo;
-    ListaTareas listaTareas;
+    string nombre; // Nombre del usuario.
+    string correo; // Correo del usuario.
+    ListaTareas listaTareas; // Lista de tareas.
 
 public:
-    void setNombre(const string& nom);
-    void setCorreo(const string& mail);
-    string getNombre() const;
-    string getCorreo() const;
-    ListaTareas& getListaTareas();
+    void setNombre(const string& nom); // Método para establecer el nombre del usuario.
+    void setCorreo(const string& mail); // Método para establecer el correo del usuario.
+    string getNombre() const; // Método para obtener el nombre del usuario.
+    string getCorreo() const; // Método para obtener el correo del usuario.
+    ListaTareas& getListaTareas(); // Método para obtener la lista de tareas del usuario.
 };
 
 #endif // USUARIO_H
