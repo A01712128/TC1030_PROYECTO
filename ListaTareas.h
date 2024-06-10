af8d52bcf46cb0524b388b750550974361ef59dd
LiliValdo <142357070+A01712128@users.noreply.github.com>
2024-06-10 10:16:12 -0600

    Update ListaTareas.h

--- ListaTareas.h
+++ ListaTareas.h
@@ -1,19 +1,31 @@
-#ifndef LISTATAREAS_H
-#define LISTATAREAS_H
+/*
+ *
+ * Proyecto Gestor de tareas clase ListaTareas
+ * Liliana Valdovinos Catarino
+ * A01712128
+ * 10/06/2024
+ *
+ * Esta clase define un objeto de tipo ListaTareas que contiene todas las 
+ * operaciones para manejar una lista de tareas. Permite agregar
+ * y mostrar tareas. Esta clase es utilizada por la clase Usuario para gestionar 
+ * las tareas de un usuario.
+ */
 
-#include "TareaBase.h"
+#ifndef LISTATAREAS_H // Si LISTATAREAS_H no ha sido definido,
+#define LISTATAREAS_H 
+
+#include "TareaBase.h" 
 #include <vector>
 
 class ListaTareas {
 private:
-    vector<TareaBase*> tareas;
+    vector<TareaBase*> tareas; // Vector de punteros a TareaBase
 
 public:
-    void agregarTarea(TareaBase* tarea);
-    void eliminarTarea(TareaBase* tarea);
-    void mostrarTareas() const;
-    vector<TareaBase*>& getTareas() { return tareas; } // Implementación en línea de getTareas()
-    ~ListaTareas();
+    void agregarTarea(TareaBase* tarea); // Método para agregar una tarea a la lista.
+    void mostrarTareas() const; // Método para mostrar todas las tareas de la lista.
+    vector<TareaBase*>& getTareas() { return tareas; } // Método para obtener todas las tareas de la lista.
+    ~ListaTareas(); // Destructor de la lista de tareas.
 };
 
 #endif // LISTATAREAS_H
