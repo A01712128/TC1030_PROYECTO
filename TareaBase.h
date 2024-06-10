58f18c709bc1aa6e1f5b850167179effadf1dab9
LiliValdo <142357070+A01712128@users.noreply.github.com>
2024-06-10 10:19:12 -0600

    Update TareaBase.h

--- TareaBase.h
+++ TareaBase.h
@@ -1,4 +1,17 @@
-#ifndef TAREABASE_H
+/*
+ *
+ * Proyecto Gestor de tareas clase TareaBase
+ * Liliana Valdovinos Catarino
+ * A01712128
+ * 10/06/2024
+ *
+ * Esta clase define un objeto abstracto de tipo TareaBase que sirve como base 
+ * para las tareas. Contiene operaciones comunes para las tareas, como marcar 
+ * una tarea como completada o incompleta, y verificar su estado. Esta clase 
+ * es utilizada como base para las clases TareaSimple y TareaProgramada.
+ */
+
+#ifndef TAREABASE_H // Si TAREABASE_H no ha sido definido,
 #define TAREABASE_H
 
 #include <string>
@@ -6,17 +19,17 @@ using namespace std;
 
 class TareaBase {
 protected:
-    string descripcion;
-    bool completada;
+    string descripcion; // Descripción de la tarea.
+    bool completada; // Estado de la tarea.
 
 public:
-    TareaBase(const string& desc);
+    TareaBase(const string& desc); // Constructor que inicia la descripción y el estado de la tarea.
 
-    virtual void marcarCompletada();
-    virtual void marcarIncompleta();
-    virtual string getDescripcion() const = 0;
-    virtual bool estaCompletada() const;
-    virtual ~TareaBase() = 0;
+    virtual void marcarCompletada(); // Método virtual para marcar la tarea como completada.
+    virtual void marcarIncompleta(); // Método virtual para marcar la tarea como incompleta.
+    virtual string getDescripcion() const = 0; // Método virtual puro para obtener la descripción de la tarea.
+    virtual bool estaCompletada() const; // Método virtual para verificar si la tarea está completada.
+    virtual ~TareaBase() = 0; // Destructor virtual puro.
 };
 
 #endif // TAREABASE_H
