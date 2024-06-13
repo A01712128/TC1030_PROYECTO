3f3b258993e42dedc8166674cb43cd8589261a2d
LiliValdo <142357070+A01712128@users.noreply.github.com>
2024-06-13 07:29:06 -0600

    Update ListaTareas.h

--- ListaTareas.h
+++ ListaTareas.h
@@ -12,20 +12,57 @@
  */
 
 #ifndef LISTATAREAS_H // Si LISTATAREAS_H no ha sido definido,
-#define LISTATAREAS_H 
+#define LISTATAREAS_H
 
-#include "TareaBase.h" 
+#include "TareaBase.h"
+#include "TareaProgramada.h" 
 #include <vector>
+#include <iostream>
+using namespace std;
 
 class ListaTareas {
 private:
     vector<TareaBase*> tareas; // Vector de punteros a TareaBase
 
 public:
-    void agregarTarea(TareaBase* tarea); // Método para agregar una tarea a la lista.
-    void mostrarTareas() const; // Método para mostrar todas las tareas de la lista.
-    vector<TareaBase*>& getTareas() { return tareas; } // Método para obtener todas las tareas de la lista.
-    ~ListaTareas(); // Destructor de la lista de tareas.
+    // Agrega una tarea a la lista 
+    void agregarTarea(TareaBase* tarea) {
+        tareas.push_back(tarea);
+    }
+
+    // Elimina una tarea de la lista
+    void eliminarTarea(TareaBase* tarea) {
+        for (auto it = tareas.begin(); it != tareas.end(); ++it) {
+            if (*it == tarea) {
+                tareas.erase(it);
+                break;
+            }
+        }
+    }
+
+    // Muestra todas las tareas en la lista
+// Const al final de la función, asegura que esta función no altere el objeto lista, ni sus miembros.
+    void mostrarTareas() const {
+        for (size_t i = 0; i < tareas.size(); ++i) {
+            cout << "[" << i + 1 << "] ";
+            cout << (dynamic_cast<TareaProgramada*>(tareas[i]) ? "Tarea Programada: " : "Tarea Simple: ");
+            cout << "Descripcion: " << tareas[i]->getDescripcion() << "\n";
+            cout << "Completada: " << (tareas[i]->estaCompletada() ? "Si" : "No") << "\n";
+            cout << "-------------------\n";
+        }
+    }
+
+    // Retorna la lista de tareas
+    vector<TareaBase*>& getTareas() {
+        return tareas;
+    }
+
+    // Destructor que libera la memoria de las tareas
+    ~ListaTareas() {
+        for (auto& tarea : tareas) {
+            delete tarea;
+        }
+    }
 };
 
 #endif // LISTATAREAS_H
